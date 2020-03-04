0ea2b0e5092e3de52ca83b0e62db1433b4e8e310
ceade <chris.eade@gmail.com>
2020-03-04 22:28:29 +0100

    Whoops

--- z8t.pl
+++ z8t.pl
@@ -256,7 +256,7 @@ sub run_test {
         `rm test.core`;
         `rm test.bin`;
     }
-    if ($self->{registers}->{SP}->{UL} !== 0xffff) {
+    if ($self->{registers}->{SP}->{UL} != 0xffff) {
         diag($self, sprintf("WARN: the stack looks pooped: 0x%04x", $self->{registers}->{SP}->{UL}));
     }
 }
